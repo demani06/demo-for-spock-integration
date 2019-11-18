0e39ea9a595f63614d20d5333baefa7d9b3cdb76
Deepak Emani <deepakemani@gmail.com>
2019-11-18 09:42:07 +0000

    working demo - iteration 2 - added lombok

--- src/main/java/com/deepak/demoforspockintegration/HelloWorldController.java
+++ src/main/java/com/deepak/demoforspockintegration/HelloWorldController.java
@@ -1,10 +1,14 @@
 package com.deepak.demoforspockintegration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private HelloWorldService helloWorldService;
 
     @GetMapping("/hello")
     public void helloWorld() {
---
+++ src/main/java/com/deepak/demoforspockintegration/HelloWorldService.java
@@ -0,0 +1,9 @@
+package com.deepak.demoforspockintegration;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWorldService {
+
+
+}
